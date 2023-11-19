7943fd2fb466e23961ff9bb7bbf081416f8f9d5d
Gijs P <gijs_son@hotmail.com>
2023-11-19 20:00:41 +0100

    Upgrades and style tweaks

--- app/models/collection_item.rb
+++ app/models/collection_item.rb
@@ -1,6 +1,10 @@
 class CollectionItem < ApplicationRecord
   has_paper_trail
-  has_many_attached :images
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_fill: [50, 50]
+  end
+
+  scope :with_images, -> { joins(:images_attachments).uniq }
 
   belongs_to :collection
   enum availability: {available: 0, unavailable: 1, sold: 2}, _default: :available
